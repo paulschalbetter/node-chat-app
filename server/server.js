b0d7f5c3a4ba747a9c26c76a68f32f13f9a903b1
root <root@srvdev01.pascha.local>
2019-01-20 14:22:37 +0100

    add socket.io message events

--- server/server.js
+++ server/server.js
@@ -15,9 +15,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Bob',
+    text: 'Hey. Can we meet tomorrow ?',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
+
 });
 
 server.listen(port, () => {
