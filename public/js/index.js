5580ce40ee8cb72df64b52cfb6516dc1ec89baed
root <root@srvdev01.pascha.local>
2019-01-20 15:49:19 +0100

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,23 @@ socket.on('connect', function () {
   console.log('Connected to server');
 });
 
-socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
-});
-
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  $('#messages').append(li);
+});
+
+$('#message-form').on('submit', function (event) {
+  event.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function () {
+
+  });
 });
--- server/server.js
+++ server/server.js
@@ -20,9 +20,9 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined.'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
-
+    callback('This is from the server');
     // io.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
